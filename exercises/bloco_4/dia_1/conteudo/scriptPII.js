000d6f17645ced5bce9ae2b9eb291af45f6c7e47
Júlio Mota <juliomotacampos@gmail.com>
2021-03-24 15:55:28 -0300

    Adicionado os exercícios de conteúdo da aula 4.1

---
+++ exercises/bloco_4/dia_1/conteudo/scriptPII.js
@@ -0,0 +1,12 @@
+const name = "Júlio Mota Campos";
+const birthCity = "Campina Grande";
+let birthYear = 1991;
+
+birthYear = 2020;
+// birthCity = "Natal";
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+console.log("Não é possível alterar o valor atribuido a cidade em que nasci, pois trata-se de uma constante!")
---
+++ exercises/bloco_4/dia_1/conteudo/scriptPIII.js
@@ -0,0 +1,25 @@
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+let arrayTest = [
+  'Júlio',
+  'Mota'
+]
+
+console.log(typeof(patientId));
+console.log(typeof(isEnrolled));
+console.log(typeof(patientInfo));
+console.log(typeof(patientEmail));
+console.log(typeof(arrayTest));
+
+let base = 5;
+let altura = 8;
+let area = (base * altura) / 2;
+let perimetro = base + base + base;
+
+console.log(area);
+console.log(perimetro);
