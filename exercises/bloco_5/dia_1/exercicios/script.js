6c8e961f21f19cdfbdabc8a9d7b69f939144df48
Júlio Mota <juliomotacampos@gmail.com>
2021-04-05 18:07:16 -0300

    Requisitos 2 e 3 - Exercício

--- exercises/bloco_5/dia_1/exercicios/script.js
+++ exercises/bloco_5/dia_1/exercicios/script.js
@@ -1,6 +1,10 @@
 let textTags = document.getElementsByTagName('p');
-let mainContentClasses = document.getElementsByClassName('main-content');
+let mainClasses = document.getElementsByClassName('main-content');
+let mainClass = mainClasses[0];
+let centerClasses = document.getElementsByClassName('center-content');
+let centerClass = centerClasses[0];
 
+// Req. 1
 function showGoalText(text) {
   return textTags[0].innerHTML = text;
 }
@@ -8,3 +12,15 @@ function showGoalText(text) {
 let goalText = 'Meus objetivos estão traçados da seguinte forma: Conseguir o primeiro estágio em 1 mês. Me efetivar em 6 meses e atuar como back-end sênior em 2 anos.'
 showGoalText(goalText);
 
+// Req 2 e 3
+function changeBackgroundColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+let mainBackgroundColor = 'rgb(76,164,109)';
+let centerBackgroundColor = 'white';
+
+changeBackgroundColor(mainClass, mainBackgroundColor);
+changeBackgroundColor(centerClass, centerBackgroundColor);
+
+// Req 4
