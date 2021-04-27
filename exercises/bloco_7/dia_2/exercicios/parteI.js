7cbe241cf9528599406f701325aa71265227db96
Júlio Mota <juliomotacampos@gmail.com>
2021-04-27 19:23:20 -0300

    Adicionado questoes 1 e 2

--- exercises/bloco_7/dia_2/exercicios/parteI.js
+++ exercises/bloco_7/dia_2/exercicios/parteI.js
@@ -35,15 +35,39 @@ const order = {
 };
 
 const customerInfo = (order) => {
-  // Adicione abaixo as informações necessárias.
+  const deliveryPersonName = order.order.delivery.deliveryPerson;
+  const customerName = order.name;
+  const customerPhone = order.phoneNumber;
+  const customerStreet = order.address.street;
+  const streetNumber = order.address.number;
+  const apartmentNumber = order.address.apartment;
 
+  console.log(`Olá ${deliveryPersonName}, entrega para ${customerName}, Telefone: ${customerPhone}, R. ${customerStreet}, N: ${streetNumber}, AP: ${apartmentNumber}`);
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
+  let customerName = order.name;
+  customerName  = 'Luiz Silva';
 
+  const foodOrder = order.order.pizza;
+  const coke = order.order.drinks.coke; 
+  foodOrder.muzzarella = { amount: 1, price: 20, }
+  foodOrder.calabresa = { amount: 1, price: 20, }
+  delete foodOrder.margherita;
+  delete foodOrder.pepperoni;
+
+  const foodOrderKeys = Object.keys(foodOrder);
+  const drinkOrderKeys = Object.keys(coke);
+
+  const price1 = order.order.pizza.muzzarella.price;
+  const price2 = order.order.pizza.calabresa.price;
+  const price3 = order.order.drinks.coke.price;
+
+  const orderPrice = price1 + price2 + price3;
+
+  console.log(`Olá ${customerName}, o total do seu pedido de ${foodOrderKeys[0]}, ${foodOrderKeys[1]} e ${drinkOrderKeys[0]} é R$ ${orderPrice}.`);
 }
 
 orderModifier(order);
