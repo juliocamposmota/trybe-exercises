8f34e12390583d947dece1bbeb5907a77ed1e011
Júlio Mota <juliomotacampos@gmail.com>
2021-04-28 14:55:36 -0300

    Adicionado exercicios 5, 6, 7 e 8

--- exercises/bloco_7/dia_2/exercicios/parteII.js
+++ exercises/bloco_7/dia_2/exercicios/parteII.js
@@ -34,3 +34,48 @@ console.log(showObjectLength(lesson1));
 // Exercício 4
 const showObjectValues = object => Object.values(object);
 console.log(showObjectValues(lesson1));
+
+// Exercício 5
+console.log("All Lessons");
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
+
+// Exercício 6
+console.log('Total de estudantes');
+const sumStudents = object => {
+  const array = Object.keys(object);
+  let sum = 0;
+  for (let index in array) {
+    sum += object[array[index]].numeroEstudantes;
+  }
+
+  return sum;
+}
+console.log(sumStudents(allLessons));
+
+// Exercício 7
+console.log('Valor da chave');
+const getValueByNumber = (object, number) => {
+  const values = Object.values(object);
+  const value = values[number];
+
+  return value;
+}
+console.log(getValueByNumber(lesson1, 0));
+
+// Exercício 8
+console.log('Existe o par?');
+const verifyPair = (object, key, value) => {
+  let existPair = false;
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+
+  for (let index in keys) {
+    if (keys[index] === key && values[index] === value) {
+      existPair = true;
+    }
+  }
+
+  return existPair;
+}
+console.log(verifyPair(lesson3, 'turno', 'noite'));
