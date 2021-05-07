0bc40b7de7a452b86d4ef5e7264b85c29a5f9dfd
Júlio Mota <juliomotacampos@gmail.com>
2021-05-07 15:49:11 -0300

    Adicionado praticas do conteudo

--- exercises/bloco_8/dia_4/conteudo/reduce.js
+++ exercises/bloco_8/dia_4/conteudo/reduce.js
@@ -16,6 +16,27 @@ const getSum = (result, number) => result + number;
 const sumNumbers2 = numbers.reduce(getSum);
 console.log(sumNumbers2);
 
-// Passando um segundo parâmetro para o reduce
+// Exemplo 2- Passando um segundo parâmetro para o reduce
 const sumNumbers3 = numbers.reduce(getSum, 10);
 console.log(sumNumbers3);
+
+// Exemplo 3 - Pegando o maior valor de um array com reduce
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+const bigger = numbers.reduce(getBigger);
+console.log(bigger);
+
+// Exemplo 4 - Somar todos os números pares do array
+const numbers2 = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const getSum2 = (sum, number) => sum + number;
+
+const sumEvens = (array) => array.filter(getEven).reduce(getSum2);
+
+console.log(sumEvens(numbers2)); // exemplo com reduce e filter
+
+const sumEvens2 = (array) => array.reduce((sum, number) => (
+  number % 2 === 0 ? sum + number : sum
+));
+
+console.log(sumEvens2(numbers2)); // exemplo só com reduce
