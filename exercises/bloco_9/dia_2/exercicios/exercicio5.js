676b4ba95235ff14f9e8a933a112a1910629199a
Júlio Mota <juliomotacampos@gmail.com>
2021-05-13 15:37:26 -0300

    Refatorada

--- exercises/bloco_9/dia_2/exercicios/exercicio5.js
+++ exercises/bloco_9/dia_2/exercicios/exercicio5.js
@@ -3,20 +3,15 @@ const divisionArray = [2, 3, 5, 10];
 const division = (number, array) => array.map((element) => number / element);
 
 const fetchPromise = () => {
-  const myPromise = new Promise((resolve, reject) => {
+  new Promise((resolve, reject) => {
     const randomNumbers = Array.from(
       { length: 10 },
       () =>  Math.floor(Math.random() * 50) + 1
     );
 
-    const squared = randomNumbers.map((number) => Math.pow(number, 2));
-    const sum = squared.reduce((acc, curr) => acc + curr);
+    const sum = randomNumbers.map((number) => Math.pow(number, 2)).reduce((acc, curr) => acc + curr);
 
-    if (sum >= 8000) {
-      return reject(sum);
-    }
-
-    resolve(sum);
+    (sum >= 8000) ? reject(sum) : resolve(sum);
   })
   .then((sum) => division(sum, divisionArray))
   .then((array) => array.reduce((acc, curr) => acc + curr))
