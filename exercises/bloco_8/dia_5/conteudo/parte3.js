7140398982ca28e8f27b58fee8cb46cb4b50e930
Júlio Mota <juliomotacampos@gmail.com>
2021-05-14 13:41:14 -0300

    Adicionado exercicio 3

--- exercises/bloco_8/dia_5/conteudo/parte3.js
+++ exercises/bloco_8/dia_5/conteudo/parte3.js
@@ -4,21 +4,21 @@ const product = {
   seller: 'Casas de Minas',
 };
 
-const values = Object.values(product); // uma forma de fazer
-console.log(values);
+// const values = Object.values(product); // uma forma de fazer
+// console.log(values);
 
-const { name, seller } = product;
-console.log(name);
-console.log(seller);
+// const { name, seller } = product;
+// console.log(name);
+// console.log(seller);
 
-const student = {
-  a: 'Maria',
-  b: 'Turma B',
-  c: 'Matematica',
-};
+// const student = {
+//   a: 'Maria',
+//   b: 'Turma B',
+//   c: 'Matematica',
+// };
 
-const { a: nome, b: turma, c: disciplina } = student;
-console.log(nome, turma, disciplina);
+// const { a: nome, b: turma, c: disciplina } = student;
+// console.log(nome, turma, disciplina);
 
 const printProductDetails = ({name, price, seller}) => {
   console.log(`Promoção! ${name} por apenas R$${price}, só na ${seller}`)
