ccb64d2f132c7e29739b7323381762454490be04
Júlio Mota <juliomotacampos@gmail.com>
2021-05-19 18:28:43 -0300

    Adicionado exercicio1

--- exercises/bloco_10/dia_1/exercicios/primeira-parte/exercicio1.spec.js
+++ exercises/bloco_10/dia_1/exercicios/primeira-parte/exercicio1.spec.js
@@ -3,6 +3,18 @@ const { sum } = require('./exercicio1');
 
 describe('A função sum: ', () => {
   it('Retorna 9 ao receber 4 e 5', () => {
-    expect(sum(4, 9)).toBe(9);
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('Retorna 0 ao receber 0 e 0', () => {
+    expect(sum(0 ,0)).toBe(0);
+  });
+
+  it('Retorna error ao receber qualquer parâmetro que não seja string', () => {
+    expect(() => { sum(4, '5') }).toThrow();
+  });
+
+  it('Retorna "parameters must be numbers" ao receber qualquer parâmetro que não seja string', () => {
+    expect(() => { sum(4, '5') }).toThrow('parameters must be numbers');
   });
 });
