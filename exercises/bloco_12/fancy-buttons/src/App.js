6812252da3e037efa1eba91aef7f2378e6c78d9e
Júlio Mota <juliomotacampos@gmail.com>
2021-05-27 16:11:43 -0300

    Adicionado estado e evento de contar cliks dos botoes

--- exercises/bloco_12/fancy-buttons/src/App.js
+++ exercises/bloco_12/fancy-buttons/src/App.js
@@ -5,22 +5,43 @@ class App extends React.Component {
   constructor(props) {
     super()
 
+    this.state = {
+      fancyCliks: 0,
+      regularCliks: 0,
+      crazyCLiks: 0,
+    }
+
     this.fancyHandle = this.fancyHandle.bind(this);
     this.regularHandle = this.regularHandle.bind(this);
     this.crazyHandle = this.crazyHandle.bind(this);
   }
 
-  fancyHandle = () => console.log('This is my fancy button');
-  regularHandle = () => console.log('This is my regular button');
-  crazyHandle = () => console.log('This is my crazy button');
+  fancyHandle = () => {
+    this.setState((estadoAnterior, _props) => ({
+      fancyCliks: estadoAnterior.fancyCliks + 1
+    }))
+  };
+
+  regularHandle = () => {
+    this.setState((estadoAnterior, _props) => ({
+      regularCliks: estadoAnterior.regularCliks + 1
+    }))
+  };
+
+  crazyHandle = () => {
+    this.setState((estadoAnterior, _props) => ({
+      crazyCLiks: estadoAnterior.crazyCLiks + 1
+    }))
+    // console.log('teste')
+  };
 
   render() {
-    
+
     return (
       <div>
-        <button onClick={this.fancyHandle}>My Fancy Button</button>
-        <button onClick={this.regularHandle}>My Regular Button</button>
-        <button onClick={this.crazyHandle}>My Crazy Button</button>
+        <button onClick={this.fancyHandle}>{`My Fancy Button counts ${this.state.fancyCliks}`}</button>
+        <button onClick={this.regularHandle}>{`My Regular Button counts ${this.state.regularCliks}`}</button>
+        <button onClick={this.crazyHandle}>{`My Crazy Button counts ${this.state.crazyCLiks}`}</button>
       </div>
     );
   }
