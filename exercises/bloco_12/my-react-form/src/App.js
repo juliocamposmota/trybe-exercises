3a28bf3a3f6e3e9c6b265d0394edfc437d05647d
Júlio Mota <juliomotacampos@gmail.com>
2021-06-04 16:18:45 -0300

    Criado construtor, handles e adicionado os eventos aos elementos do formulario

--- exercises/bloco_12/my-react-form/src/App.js
+++ exercises/bloco_12/my-react-form/src/App.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemonState: 'Charmander',
+      nameState: '',
+      ageState: 0,
+    }
+
+    this.pokemonHandle = this.pokemonHandle.bind(this);
+    this.nameHandle = this.nameHandle.bind(this);
+    this.ageHandle = this.ageHandle.bind(this);
+  }
+
+  pokemonHandle(event) {
+    this.setState({
+      pokemonState: event.target.value,
+    })
+  }
+
+  nameHandle(event) {
+    this.setState({
+      nameState: event.target.value,
+    })
+  }
+
+  ageHandle(event) {
+    this.setState({
+      ageState: event.target.value,
+    })
+  }
+
   render() {
 
     return (
@@ -12,7 +44,7 @@ class App extends Component {
           <label>
             Choose your first Pokemon: 
 
-            <select>
+            <select onChange={this.pokemonHandle}>
               <option value="charmander">Charmander</option>
               <option value="bulbassaur">Bulbassaur</option>
               <option value="squirtle">Squirtle</option>
@@ -22,12 +54,12 @@ class App extends Component {
           <label>
             Adventure name:
 
-            <input type="text" name="nome" />
+            <input type="text" name="nome" onChange={this.nameHandle} />
           </label>
 
           <label>
             Age:
-            <input type="number" name="idade" />
+            <input type="number" name="idade" onChange={this.ageHandle} />
           </label>
         </form>
       </div>
