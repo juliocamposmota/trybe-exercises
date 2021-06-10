ea345f9e9747d47dc677555b5216cbfda715aae5
Júlio Mota <juliomotacampos@gmail.com>
2021-06-10 18:36:40 -0300

    Renderiza loading ao carregar a pagina

--- exercises/bloco_13/random-user-app/src/App.js
+++ exercises/bloco_13/random-user-app/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import PersonalData from './components/PersonalData';
+import Loading from './components/Loading';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      users: [],
+    }
+
+    this.fetchUsers = this.fetchUsers.bind(this);
+  }
+
+  async fetchUsers() {
+    const requestHeaders = { headers: { Accept: 'aplication/json' } }
+    const requestReturn = await fetch('https://api.randomuser.me/', requestHeaders);
+    const requestObject = await requestReturn.json();
+    const results = await requestObject.results;
+
+    this.setState({
+      loading: false,
+      users: results,
+    })
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
   render() {
+    const { loading, users } = this.state;
+
     return (
       <div>
-        <PersonalData />
+        { loading ? <Loading /> : <PersonalData users={ users }/> }
       </div>
     );
   }
--- exercises/bloco_13/random-user-app/src/components/PersonalData.jsx
+++ exercises/bloco_13/random-user-app/src/components/PersonalData.jsx
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
 
 class PersonalData extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      users: [],
-    }
-
-    this.fetchUsers = this.fetchUsers.bind(this);
-  }
-
-  async fetchUsers() {
-    const requestHeaders = { headers: { Accept: 'aplication/json' } }
-    const requestReturn = await fetch('https://api.randomuser.me/', requestHeaders);
-    const requestObject = await requestReturn.json();
-    const results = await requestObject.results;
-
-    this.setState({
-      users: results,
-    })
-  }
-
-  componentDidMount() {
-    this.fetchUsers();
-  }
-
   render() {
-    const { users } = this.state;
+    const { users } = this.props;
 
     return (
       <div>
         { users.map((user) => (
           <div key={ user.login.uuid }>
+            <img src={user.picture.large} alt="random user profile" />
             <p>{ user.name.first }</p>
+            <p>{ user.email }</p>
+            <p>{ user.dob.age }</p>
+            <p>{ user.location.country }</p>
           </div>
         )) }
       </div>
