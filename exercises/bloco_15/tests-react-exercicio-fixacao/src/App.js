c4ebb1f5fa750e142fc4e7fbb7a57dcf039cc99a
Júlio Mota <juliomotacampos@gmail.com>
2021-06-25 16:23:41 -0300

    Cria o component ValidEmail e seus testes

--- exercises/bloco_15/tests-react-exercicio-fixacao/src/App.js
+++ exercises/bloco_15/tests-react-exercicio-fixacao/src/App.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
+import ValidEmail from './ValidEmail';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      savedEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({
+      email: value,
+    });
+  }
+
+  changeSavedEmail(value) {
+    this.setState({
+      savedEmail: value,
+      email: '',
+    });
+  }
+
   render() {
+    const { email, savedEmail } = this.state;
+
     return (
-      <div>
+      <div className="app">
         <p>App component</p>
+        
+        <label htmlFor="id-email">
+          Email
+        </label>
+
+        <input
+          id="id-email"
+          type="email"
+          value={ email }
+          onChange={ ({target}) => this.changeEmail(target.value)}
+        />
+
+        <input
+          data-testid="send-button"
+          type="button"
+          value="Enviar"
+          onClick={ () => this.changeSavedEmail(email) }
+        />
+
+        <button data-testid="back-button" type="button">Voltar</button>
+        <button data-testid="clear-button" type="button">Apagar</button>
+
+        <ValidEmail email={savedEmail} />
       </div>
     );
   }
--- exercises/bloco_15/tests-react-exercicio-fixacao/src/App.test.js
+++ exercises/bloco_15/tests-react-exercicio-fixacao/src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('if renders app component', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/app component/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('if renders an input email', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('if renders 3 buttons', () => {
+  const { getAllByRole } = render(<App />);
+  const buttons = getAllByRole('button');
+  expect(buttons.length).toBe(3);
+});
+
+test('if renders a send button', () => {
+  const { getByTestId } = render(<App />);
+  const buttonSend = getByTestId('send-button');
+  expect(buttonSend.value).toBe('Enviar');
+});
+
+test('if renders a back button', () => {
+  const { getByTestId } = render(<App />);
+  const buttonBack = getByTestId('back-button');
+  expect(buttonBack.innerHTML).toBe('Voltar');
+});
+
+test('if renders a clear button', () => {
+  const { getByTestId } = render(<App />);
+  const buttonClear = getByTestId('clear-button');
+  expect(buttonClear.innerHTML).toBe('Apagar');
+});
+
+test('if email button or field are working', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+  const EMAIL_USER = 'nomedoemail@empresaprovedora.com';
+
+  const textEmail = getByTestId('saved-email');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Saved');
+
+  const buttonSend = getByTestId('send-button');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(buttonSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Saved ${EMAIL_USER}`);
 });
