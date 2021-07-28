6976910dfdad3a725159eba15e5dd1c44674dfda
Júlio Mota <juliomotacampos@gmail.com>
2021-07-28 16:26:17 -0300

    Adiciona store

--- exercises/bloco_16/dia_3/doguinhos-app/src/App.js
+++ exercises/bloco_16/dia_3/doguinhos-app/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
 
 class App extends Component {
   render() {
+    const { isFetching, src, fetchDog } = this.props;
+
     return (
-      <div>
-        Doguinhos App
-      </div>
+      isFetching ? <p>Loading...</p>
+      : (
+        <div style={{ width: 500 }}>
+          <button
+            style={{ display: 'block' }}
+            onClick={ fetchDog }
+            type="button"
+          >
+            Novo doguinho
+          </button>
+          <img
+            style={{ width: '100%' }}
+            src={ src }
+            alt="dog"
+          />
+        </div>
+      )
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
