cdc20bb67f886773a82d88faa942c3ca7a193930
Júlio Mota <juliomotacampos@gmail.com>
2021-07-29 15:07:01 -0300

    Adiciona render with redux e muda store para index

---
+++ exercises/bloco_16/dia_5/contador-redux-testes/src/renderWithRedux.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux';
+import clickReducer from './reducers';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+export default renderWithRedux;
