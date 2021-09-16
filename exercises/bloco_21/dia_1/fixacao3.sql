fcdfbe8ff4baec7dbc9a35fbb0cdf49d7b07a71a
Júlio Mota <juliomotacampos@gmail.com>
2021-09-16 15:05:25 -0300

    Adiciona exercicio de fixacao 3

---
+++ exercises/bloco_21/dia_1/fixacao3.sql
@@ -0,0 +1,5 @@
+SELECT UCASE('trybe');
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+SELECT CHAR_LENGTH('Uma frase qualquer');
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 12, 11);
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
