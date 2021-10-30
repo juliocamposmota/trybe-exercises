fbe516f250517091b7a582893cf641523bc4f980
Júlio Mota <juliomotacampos@gmail.com>
2021-10-30 09:25:13 -0300

    adiciona exercicio 3

--- exercises/bloco_25/dia_1/exercicios/imc.js
+++ exercises/bloco_25/dia_1/exercicios/imc.js
@@ -1,5 +1,7 @@
-const weight = 80;
-const height = 178;
+const readline = require('readline-sync');
+
+const weight = readline.question('Qual seu peso?');
+const height = readline.question('Qual sua altura?');
 
 const calculateImc = (weight, height) => {
   const imc = (weight / Math.pow(height / 100, 2)).toFixed(2);
