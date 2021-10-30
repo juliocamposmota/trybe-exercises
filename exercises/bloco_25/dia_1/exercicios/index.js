c842c8bd3acaf6a2f7f7266b643d47a3c214ef16
Júlio Mota <juliomotacampos@gmail.com>
2021-10-30 10:46:50 -0300

    adiciona exercicio 8

--- exercises/bloco_25/dia_1/exercicios/index.js
+++ exercises/bloco_25/dia_1/exercicios/index.js
@@ -1,1 +1,21 @@
-const calcImc = require('./imc');
+const readline = require('readline-sync');
+
+const scripts = [
+  { name: 'Calcular IMC', script: './imc.js' },
+  { name: 'Calcular velocidade média', script: './velocidade.js' },
+  { name: 'Jogo de Adivinhação', script: './sorteio.js' },
+];
+
+let message = scripts.map((script, i) => {
+  return `${i + 1} - ${script.name}`;
+});
+
+message = message.join('\n');
+console.log(message);
+
+const scriptNumber = readline.questionInt('Escolha um script pelo número: ') - 1;
+const script = scripts[scriptNumber];
+
+if(!script) return console.log('Script inválido. Fui...');
+
+require(script.script);
