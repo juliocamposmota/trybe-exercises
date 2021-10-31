4cc0c6728df2fafcfd62ddb65e418a7a3ed95522
Júlio Mota <juliomotacampos@gmail.com>
2021-10-31 11:17:53 -0300

    conclui exercicio 4

--- exercises/bloco_25/dia_2/exercicios/exercicio4.js
+++ exercises/bloco_25/dia_2/exercicios/exercicio4.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
 
 const fileName = 'simpsons.json';
+const newFileName = 'simpsonsFamily.json';
 
 const showCharacters = async () => {
   try {
@@ -15,10 +16,109 @@ const showCharacters = async () => {
   }
 };
 
-showCharacters();
+// showCharacters();
 
-const showCharactersById = (id) => {
-  return new Promise((resolve, reject) => {
-    
-  });
+const showCharactersById = async (characterId) => {
+  const characters = await fs
+    .readFile(fileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const result = characters
+    .find((char) => char.id == characterId);
+
+  if (!result) throw new Error('id não encontrado');
+
+  console.log(result);
+  return result
 };
+
+// showCharactersById(10);
+
+const removeCharacters = async () => {
+  const characters = await fs
+    .readFile(fileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const TEN = '10';
+  const SIX = '6';
+
+  const result = characters
+    .filter(({ id }) => id != TEN && id != SIX);
+
+  console.log(result);
+  return result
+};
+
+// removeCharacters();
+
+const createSimpsonsFamily = async () => {
+  const characters = await fs
+    .readFile(fileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const familyArray = ['1', '2', '3', '4'];
+
+  const result = characters
+    .filter(({ id }) => familyArray.includes(id));
+
+  console.log(result);
+
+  await fs.writeFile(
+    newFileName,
+    JSON.stringify(result)
+  ).then(() => console.log('Arquivo salvo.'));
+};
+
+// createSimpsonsFamily();
+
+const addNelsonToSimpsons = async () => {
+  const characters = await fs
+    .readFile(fileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const simpsons = await fs
+    .readFile(newFileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const nelsonId = '8';
+  const result = characters
+    .find(({ id }) => id == nelsonId);
+
+  simpsons.push(result);
+
+  console.log(simpsons);
+
+  await fs.writeFile(
+    newFileName,
+    JSON.stringify(simpsons)
+  ).then(() => console.log('Arquivo salvo.'));
+};
+
+// addNelsonToSimpsons();
+
+const replaceNelsonForMaggie = async () => {
+  const characters = await fs
+    .readFile(fileName, 'utf-8')
+    .then((data) => JSON.parse(data));
+
+  const nelsonId = '8';
+  const simpsons = await fs
+    .readFile(newFileName, 'utf-8')
+    .then((data) => JSON.parse(data))
+    .then((data) => data.filter(({ id }) => id != nelsonId));
+
+  const maggieId = '5';
+  const maggie = characters
+    .find(({ id }) => id == maggieId);
+
+    
+  simpsons.push(maggie);
+  console.log(simpsons);
+
+  await fs.writeFile(
+    newFileName,
+    JSON.stringify(simpsons)
+  ).then(() => console.log('Arquivo salvo.'));
+};
+
+replaceNelsonForMaggie();
