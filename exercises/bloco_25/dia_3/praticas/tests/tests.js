147c5ef436986d0cd0517a37bc1867cd4a9871b9
Júlio Mota <juliomotacampos@gmail.com>
2021-11-04 18:42:06 -0300

    adiciona stubs para arquivo e finaliza tdd

--- exercises/bloco_25/dia_3/praticas/tests/tests.js
+++ exercises/bloco_25/dia_3/praticas/tests/tests.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const sinon = require('sinon');
 const { expect } = require('chai');
 const leArquivo = require('../functions/leArquivo');
 
@@ -5,20 +7,38 @@ const CONTEUDO = 'VQV com TDD';
 
 describe('leArquivo', () => {
   describe('Quando o arquivo existe', () => {
-    describe('a resposta', () => {
-      const resposta = leArquivo('arquivo.txt');
+    before(() => {
+      sinon.stub(fs, 'readFileSync').returns(CONTEUDO);
+    });
 
+    after(() => {
+      fs.readFileSync.restore()
+    });
+
+    describe('a resposta', () => {
       it('é uma string', () => {
+        const resposta = leArquivo('arquivo.txt');
         expect(resposta).to.be.a('string');
       });
 
       it('é igual ao conteúdo do arquivo', () => {
+        const resposta = leArquivo('arquivo.txt');
         expect(resposta).to.be.equals(CONTEUDO);
       });
     });
   });
 
   describe('Quando o arquivo não existe', () => {
+    before(() => {
+      sinon
+        .stub(fs, 'readFileSync')
+        .throws(new Error('Arquivo não encontrado'));
+    });
+
+    after(() => {
+      fs.readFileSync.restore()
+    });
+
     describe('a resposta', () => {
       it('é igual a "null"', () => {
         const resposta = leArquivo('arquivo_nao_existe.txt');
