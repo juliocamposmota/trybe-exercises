61df21be4cdde869ba7def88d6737d7be45d45d7
Júlio Mota <juliomotacampos@gmail.com>
2021-11-05 17:38:44 -0300

    Estrutura API de receitas

--- exercises/bloco_25/dia_4/hello-express/index.js
+++ exercises/bloco_25/dia_4/hello-express/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
-
-const PORT = 3001;
+const sortByName = require('./functions/sortByName');
 
 const app = express();
+const PORT = 3001;
 
-app.get('/hello', handleRequest);
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
 
-function handleRequest(req, res) {
-  res.status(200).send('Hello World');
-}
+app.get('/recipes', function (req, res) {
+  const sortedRecipes = sortByName(recipes);
+  res.json(sortedRecipes);
+});
+
+app.get('/drinks', function (req, res) {
+  const sortedDrinks = sortByName(drinks);
+  res.json(sortedDrinks);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+});
