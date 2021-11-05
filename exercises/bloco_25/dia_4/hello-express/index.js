2fcdb7e325585f60412dbf93f1ec58b2ff502179
Júlio Mota <juliomotacampos@gmail.com>
2021-11-05 17:46:56 -0300

    parametros na rota

--- exercises/bloco_25/dia_4/hello-express/index.js
+++ exercises/bloco_25/dia_4/hello-express/index.js
@@ -24,9 +24,27 @@ app.get('/recipes', function (req, res) {
   res.json(sortedRecipes);
 });
 
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found' });
+
+  res.status(200).json(recipe);
+});
+
 app.get('/drinks', function (req, res) {
   const sortedDrinks = sortByName(drinks);
   res.json(sortedDrinks);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((r) => r.id === parseInt(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found' });
+
+  res.status(200).json(drink);
 });
 
 app.listen(PORT, () => {
