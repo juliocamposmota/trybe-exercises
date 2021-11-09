a29e49c58722dd6e2606e108ceb338328e3b5901
Júlio Mota <juliomotacampos@gmail.com>
2021-11-09 14:31:51 -0300

    adiciona exercicio 2

--- exercises/bloco_25/dia_4/exercicios/index.js
+++ exercises/bloco_25/dia_4/exercicios/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-
-const app = express();
+const bodyparser = require('body-parser');
 
 const PORT = 3001;
 
+const app = express();
+app.use(bodyparser.json());
+
 app.get('/ping', (_req, res) => {
   return res.json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  return res.json({ message: `Hello, ${name}` });
 });
 
 app.listen(PORT, () => {
