0f2c976356504256d9a5af4500c3706d7e7fd2f6
Júlio Mota <juliomotacampos@gmail.com>
2021-11-09 16:48:56 -0300

    adiciona api simpsons

--- exercises/bloco_25/dia_4/exercicios/index.js
+++ exercises/bloco_25/dia_4/exercicios/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const fs = require('fs');
 
 const PORT = 3001;
+const DATABASE = 'simpsons.json';
 
 const app = express();
 app.use(bodyparser.json());
@@ -24,6 +26,49 @@ app.post('/greetings', (req, res) => {
 app.put('/users/:name/:age', (req, res) => {
   const { name, age }= req.params;
   return res.json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+app.get('/simpsons', (_req, res) => {
+  const data = fs.readFileSync(DATABASE);
+  const characters = JSON.parse(data);
+
+  if (!data) return res.status(500).json({
+    message: 'ai caramba... something got wrong :/'
+  });
+
+  return res.status(200).json(characters);
+});
+
+app.get('/simpsons/:id', (req, res) => {
+  const { id } = req.params;
+
+  const data = fs.readFileSync(DATABASE);
+  const characters = JSON.parse(data);
+  
+  const character = characters.find((c) => c.id === id);
+
+  if(!data) return res.status(404).json({
+    message: 'ai caramba... character not found :/'
+  });
+
+  return res.status(200).json(character);
+});
+
+app.post('/simpsons', (req, res) => {
+  const { id, name } = req.body;
+
+  const data = fs.readFileSync(DATABASE);
+  const characters = JSON.parse(data);
+
+  const isExisting = characters.some((c) => c.id === id);
+  
+  if (isExisting) return res.json({ message: 'id already exists' });
+
+  const newCharacter = { id, name };
+
+  characters.push(newCharacter);
+  fs.writeFileSync(DATABASE, JSON.stringify(characters));
+  res.status(204).end();
 });
 
 app.listen(PORT, () => {
