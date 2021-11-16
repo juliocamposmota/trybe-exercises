416d2e8ed8487a75b0def7593347b2769f40fe17
Júlio Mota <juliomotacampos@gmail.com>
2021-11-16 19:06:40 -0300

    adiciona exercicio 4

--- exercises/bloco_26/dia_3/exercicios/controllers/productController.js
+++ exercises/bloco_26/dia_3/exercicios/controllers/productController.js
@@ -6,35 +6,53 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
+  if (!products) {
+    return res.status(500).send({ message: 'produtos não encontrados' });
+  }
+
   res.status(200).json(products);
 });
 
 router.get('/:id', async (req, res, next) => {
-  const product = await ProductModel.getById(req.params.id);
+  const { id } = req.params;
 
-  res.status(200).json(product);
+  try {
+    const product = await ProductModel.getById(id);
+    res.status(200).json({product});
+  } catch (error) {
+    res.status(500).send({ message: 'produto não encontrado' });
+  }
 });
 
 router.post('/', async (req, res) => {
-  const { name, brand } = req.body;
+  const { name, brand } = await req.body;
 
-  const newProduct = await ProductModel.add(name, brand);
-
-  res.status(201).json(newProduct);
+  try {
+    const newProduct = await ProductModel.add(name, brand);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(404).send({ message: 'não foi possível criar novo produto' });
+  }
 });
 
 router.delete('/:id', async (req, res) => {
-  const products = await ProductModel.exclude(req.params.id);
-
-  res.status(200).send(products);
+  try {
+    const products = await ProductModel.exclude(req.params.id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).send({ message: 'não foi possível deletar produto' });
+  }
 });
 
 router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
-  const products = await ProductModel.update(req.params.id, name, brand);
-
-  res.status(200).send(products);
+  try {
+    const products = await ProductModel.update(req.params.id, name, brand);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).send({ message: 'não foi possível atualizar produto' });
+  }
 });
 
 module.exports = router;
