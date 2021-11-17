e9ca60972abae14d78f86763f42934d96ebb09bb
Júlio Mota <juliomotacampos@gmail.com>
2021-11-17 15:50:29 -0300

    implementa mock e duble para criacao no mongodb

--- exercises/bloco_26/dia_4/praticas/tests/models/movieModelCreate.test.js
+++ exercises/bloco_26/dia_4/praticas/tests/models/movieModelCreate.test.js
@@ -1,16 +1,34 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
 
-// objeto simulado
-const MoviesModel = {
-  create: () => {},
-};
+const mongoConnection = require('../../models/connection');
+const MoviesModel = require('../../models/movieModels');
 
 describe('Insere um novo filme ao BD', () => {
+  let connectionMock;
+
   const payloadMovie = {
     title: 'Harry Example and the Movies',
     directedBy: 'Dobby Elf',
     releaseYear: 1999,
   };
+
+  before(() => {
+    const ID_EXAMPLE = '604cb554311d68f491ba5781';
+    const insertOne = async () => ({ insertedId: ID_EXAMPLE });
+    const collection = async () => ({ insertOne });
+    const db = async (databaseName) => ({ collection });
+    const getConnectionMock = async () => ({ db });
+
+    connectionMock = getConnectionMock()
+      .then((conn) => conn.db('model_example'));
+
+    sinon.stub(mongoConnection, 'getConnection').resolves(connectionMock);
+  });
+
+  after(() => {
+    mongoConnection.getConnection.restore();
+  });
 
   describe('Quando é inserido com sucesso', () => {
     it('retorna um objeto', async () => {
