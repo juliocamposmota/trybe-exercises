1daa5c87064928719a2897cc73993e8cbfdebbe5
Júlio Mota <juliomotacampos@gmail.com>
2021-11-24 18:59:59 -0300

    adiciona exercicio 2

--- exercises/bloco_27/dia_1/exercicios/hello-jwt/controllers/login.js
+++ exercises/bloco_27/dia_1/exercicios/hello-jwt/controllers/login.js
@@ -10,16 +10,25 @@ const validateBody = (body) => joi.object({
 }).validate(body);
 
 module.exports = (req, res, next) => {
+  const { username, password } = req.body;
   const { error } = validateBody(req.body);
 
   if (error) return next(error);
 
+  if (username === 'admin' && password !== 's3nh4S3gur4???') {
+    const err = new Error('Invalid username or password');
+    err.statusCode = 401;
+    return next(err);
+  }
+
+  const admin = username === 'admin' && password === 's3nh4S3gur4???';
+
   const payload = {
-    username: req.body.username,
-    admin: false,
+    username,
+    admin,
   };
 
   const token = jwt.sign(payload, JWT_SECRET, JWT_CONFIG);
-  
+
   res.status(200).json({ token });
 };
