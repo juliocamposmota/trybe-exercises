e882de3fbcdfb6b8a41db6d2fda11affde48f127
Júlio Mota <juliomotacampos@gmail.com>
2021-11-24 14:45:46 -0300

    inclui alterações

--- exercises/bloco_26/dia_4/exercicios/tests/controllers/movieControllerGetAll.test.js
+++ exercises/bloco_26/dia_4/exercicios/tests/controllers/movieControllerGetAll.test.js
@@ -1,15 +1,16 @@
 const sinon = require('sinon');
+
 const { expect } = require('chai');
+const { request, response } = require('express');
 
 const MoviesController = require('../../controllers/movieController');
 const MoviesServices = require('../../services/movieService');
-const { request, response } = require('express');
 
 describe('Ao chamar o controller de getAll', () => {
   describe('quando não existem filmes no banco de dados', async () => {
     const request = {};
     const response = {};
-    
+
     before(async () => {
       request.body = {};    
       
@@ -18,31 +19,31 @@ describe('Ao chamar o controller de getAll', () => {
       
       sinon.stub(MoviesServices, 'getAll').resolves([]);
     });
-    
+
     after(() => {
       MoviesServices.getAll.restore();
     });
-    
+
     it('é chamado o método "status" passando o código 200', async () => {
       await MoviesController.getAll(request, response);
       
       expect(response.status.calledWith(200)).to.be.equal(true);
       
     });
-    
+
     it('é chamado o método "json" passando uma array', async () => {
       await MoviesController.getAll(request, response);
       
       expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
     });
-    
+
     it('é chamado o método "json" passando uma array vazia', async () => {
       await MoviesController.getAll(request, response);
       
       expect(response.json.calledWith([])).to.be.equal(true);
     });
   });
-  
+
   describe('quando existem filmes no banco de dados', () => {
     const request = {};
     const response = {};
@@ -54,7 +55,7 @@ describe('Ao chamar o controller de getAll', () => {
         releaseYear: 1999,
       }
     ];
-    
+
     before(() => {
       request.body = {};
       
@@ -63,17 +64,17 @@ describe('Ao chamar o controller de getAll', () => {
       
       sinon.stub(MoviesServices, 'getAll').resolves(movies);
     });
-    
+
     after(() => {
       MoviesServices.getAll.restore();
     });
-    
+
     it('é chamado o método "status" passando o código 200', async () => {
       await MoviesController.getAll(request, response);
       
       expect(response.status.calledWith(200)).to.be.equal(true);
     });
-    
+
     it('é chamado o método "json" passando uma array', async () => {
       await MoviesController.getAll(request, response);
       
