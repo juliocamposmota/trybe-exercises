24212a15cea0b714e471afd473a415279a3aa0c9
Júlio Mota <juliomotacampos@gmail.com>
2021-11-25 18:47:01 -0300

    adiciona exercicio de fixacao

--- exercises/bloco_27/dia_2/praticas/io-multer/index.js
+++ exercises/bloco_27/dia_2/praticas/io-multer/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const path = require('path');
 
 const { PORT } = process.env;
 
@@ -21,11 +22,18 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/uploads'));
+app.use(express.static(path.resolve(__dirname, '/envios')));
 
 const upload = multer({ dest: 'uploads' });
+const envios = multer({ dest: 'envios' });
 
 app.post('/files/upload', upload.single('file'), (req, res) =>
+  res.status(200).json({ body: req.body, file: req.file })
+);
+
+app.post('/envios', envios.single('file'), (req, res) =>
   res.status(200).json({ body: req.body, file: req.file })
 );
 
