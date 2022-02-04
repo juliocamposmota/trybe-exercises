3022371df620d4366be872c37a734f0b21c42615
Júlio Mota <juliomotacampos@gmail.com>
2022-02-04 07:42:02 -0300

    added transactions

--- exercises/bloco_30/dia_3/pratica/indexUnmanaged.js
+++ exercises/bloco_30/dia_3/pratica/indexUnmanaged.js
@@ -1,22 +1,13 @@
 const express = require('express');
+const Sequelize = require('sequelize');
 const bodyParser = require('body-parser');
+
+const config = require('./config/config');
 const { Address, Employee, User, Book } = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
-
-app.get('/employees', async (_req, res) => {
-  try {
-    const employees = await Employee.findAll({
-      include: { model: Address, as: 'addresses' },
-    });
-
-    return res.status(200).json(employees);
-  } catch (e) {
-    console.log(e.message);
-    res.status(500).json({ message: 'Ocorreu um erro' });
-  };
-});
+const sequelize = new Sequelize(config.development);
 
 app.get('/employees/:id', async (req, res) => {
   try {
@@ -57,21 +48,26 @@ app.get('/usersbooks/:id', async (req, res) => {
 });
 
 app.post('/employees', async (req, res) => {
+  const t = await sequelize.transaction();
+
   try {
     const { firstName, lastName, age, city, street, number } = req.body;
 
-    const result = await sequelize.transaction(async (t) => {
-      const employee = await Employee.create({
-        firstName, lastName, age
-      }, { transaction: t });
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
 
-      await Address.create({
-        city, street, number, employeeId: employee.id
-      }, { transaction: t });
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
 
-      return res.status(201).json({ message: 'Cadastrado com sucesso' });
-    });
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
   } catch (e) {
+    await t.rollback();
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
   }
@@ -81,3 +77,25 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
 module.exports = app;
+
+
+// app.post('/employees', async (req, res) => {
+//   try {
+//     const { firstName, lastName, age, city, street, number } = req.body;
+
+//     const result = await sequelize.transaction(async (t) => {
+//       const employee = await Employee.create({
+//         firstName, lastName, age
+//       }, { transaction: t });
+
+//       await Address.create({
+//         city, street, number, employeeId: employee.id
+//       }, { transaction: t });
+
+//       return res.status(201).json({ message: 'Cadastrado com sucesso' });
+//     });
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ message: 'Algo deu errado' });
+//   }
+// });
