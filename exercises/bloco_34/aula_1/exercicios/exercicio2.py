8e41babbc6025ecbf2ccaa16d877b7ce96a6e2e9
Júlio Mota <juliomotacampos@gmail.com>
2022-04-05 20:52:04 -0300

    adiciona exercicio 2

---
+++ exercises/bloco_34/aula_1/exercicios/exercicio2.py
@@ -0,0 +1,16 @@
+"""
+Nome da abstração
+Triângulo
+
+atributos/estados
+- base (tamanho)
+- altura (tamanho)
+- aresta (quantidade)
+- vertice (quantidade)
+
+comportamentos
+- calcular area | (b * h)/2
+- informar quantidade de arestas
+- informar quantidade de vertices
+
+"""
