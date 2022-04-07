400d8a59722da01884b40ce6ebf9b0fc9f0accea
Júlio Mota <juliomotacampos@gmail.com>
2022-04-06 21:48:31 -0300

    adiciona classe compressor e a extende para os compressores gz e zip

--- exercises/bloco_34/aula_2/praticas/GzCompressor.py
+++ exercises/bloco_34/aula_2/praticas/GzCompressor.py
@@ -1,10 +1,8 @@
 import gzip
+from Compressor import Compressor
 
 
-class GzCompressor():
-    def __init__(self, filepath='./'):
-        self.filepath = filepath
-
+class GzCompressor(Compressor):
     def compress(self, file_name):
         with open(file_name, 'rb') as content:
             with gzip.open(file_name + '.gz', 'wb') as gzip_file:
--- exercises/bloco_34/aula_2/praticas/ZipCompressor.py
+++ exercises/bloco_34/aula_2/praticas/ZipCompressor.py
@@ -1,10 +1,8 @@
 from zipfile import ZipFile
+from Compressor import Compressor
 
 
-class ZipCompressor():
-    def __init__(self, filepath='./'):
-        self.filepath = filepath
-
+class ZipCompressor(Compressor):
     def compress(self, file_name):
         with ZipFile(file_name + '.zip', 'w') as zip_file:
             zip_file.write(file_name)
