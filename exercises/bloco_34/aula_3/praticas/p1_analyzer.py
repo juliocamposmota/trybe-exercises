66c315c89e6e42668982d3f3eba201bea03f18e3
Júlio Mota <juliomotacampos@gmail.com>
2022-04-08 07:25:00 -0300

    adiciona praticas de Liskov substitution principle

---
+++ exercises/bloco_34/aula_3/praticas/p1_analyzer.py
@@ -0,0 +1,10 @@
+import analyzer
+
+
+def analyze_data(token, database, data):
+    try:
+        report = analyzer.complete_report(data)
+        database.count_request(token)
+        return report
+    except analyzer.InvalidDataException:
+        return
